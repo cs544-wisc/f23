6eda507615e0a216c6d63eadb0d95a12d6cfaa82
Seth <sockerman@wisc.edu>
2023-10-16 01:17:44 +0000

    fixed import conflict

--- p4/autograde.py
+++ p4/autograde.py
@@ -8,7 +8,7 @@ from io import StringIO
 import docker
 import pandas as pd
 
-from tester import init, test, cleanup, tester_main
+from tester import init, test, tester_main
 
 # key=num, val=answer (as string)
 ANSWERS = {}
@@ -195,159 +195,164 @@ def q1():
     if not "Live datanodes (2):" in ANSWERS[1]:
         return "Q1 output does not indicate 2 live datanodes"
 
-@test(points = 10, timeout = 100)
-def basic_setup_test():
-    pwd = False
-    docker = False
-    wget = False
-    for cell in parta_json_content[0]['cells']:
-        if cell["cell_type"]  == "code":
-            if "1.1" in cell['source']:
-                if "/\r\n" in cell['outputs'][0]['text']:
-                    pwd = True
-            if "1.2" in cell['source']:  
-                if "/usr/bin/sh: 1: docker: not found" in cell['outputs'][0]['text']:
-                    docker = True
-            if "1.3" in cell['source']:
-                for line in cell['outputs'][0]['text'].split("\n"):
-                    if "hdma-wi-2021.csv" in line:
-                        wget = True
+@test(points=10)
+def q2():
+    if not 2 in ANSWERS:
+        raise Exception("Answer to question 2 not found")
+    
+# @test(points = 10, timeout = 100)
+# def basic_setup_test():
+#     pwd = False
+#     docker = False
+#     wget = False
+#     for cell in parta_json_content[0]['cells']:
+#         if cell["cell_type"]  == "code":
+#             if "1.1" in cell['source']:
+#                 if "/\r\n" in cell['outputs'][0]['text']:
+#                     pwd = True
+#             if "1.2" in cell['source']:  
+#                 if "/usr/bin/sh: 1: docker: not found" in cell['outputs'][0]['text']:
+#                     docker = True
+#             if "1.3" in cell['source']:
+#                 for line in cell['outputs'][0]['text'].split("\n"):
+#                     if "hdma-wi-2021.csv" in line:
+#                         wget = True
             
-    if pwd and wget and docker:
-        return None
-    else: 
-        return f"Failed (0/10): Failed tests marked false -- 1. pwd test: {pwd}, 1.2 docker test: {docker}, 1.3 wget test: {wget}"
+#     if pwd and wget and docker:
+#         return None
+#     else: 
+#         return f"Failed (0/10): Failed tests marked false -- 1. pwd test: {pwd}, 1.2 docker test: {docker}, 1.3 wget test: {wget}"
 
-@test(points = 15, timeout = 100)
-def hdfs_setup_test():
-    rep_settings = False
-    block_size = False
-    for cell in parta_json_content[0]['cells']:
-        if cell["cell_type"]  == "code":
+# @test(points = 15, timeout = 100)
+# def hdfs_setup_test():
+#     rep_settings = False
+#     block_size = False
+#     for cell in parta_json_content[0]['cells']:
+#         if cell["cell_type"]  == "code":
            
-            if "1.4" in cell['source']:
+#             if "1.4" in cell['source']:
 
-                double = False
-                single = False
-                for line in cell['outputs'][0]['text'].split("\n"):
-                    if 'single.csv' in line:
-                        if "166.8" in line and line.count("166.8") == 2:
-                            single = True
-                    if 'double.csv' in line:
-                        if '333.7' in line:
-                            double = True
-                rep_settings = single and double
+#                 double = False
+#                 single = False
+#                 for line in cell['outputs'][0]['text'].split("\n"):
+#                     if 'single.csv' in line:
+#                         if "166.8" in line and line.count("166.8") == 2:
+#                             single = True
+#                     if 'double.csv' in line:
+#                         if '333.7' in line:
+#                             double = True
+#                 rep_settings = single and double
             
         
-            if "1.5" in cell['source']:
-                if '1048576' in cell['outputs'][1]['text'] and '1048576' in cell['outputs'][3]['text']:
-                    block_size = True
-    if rep_settings and block_size:
-        return None
-    else:
-        return f"Failed (0/15): Failed tests marked false -- 1.4 replication settings check: {rep_settings}, 1.5 block size check: {block_size}"
+#             if "1.5" in cell['source']:
+#                 if '1048576' in cell['outputs'][1]['text'] and '1048576' in cell['outputs'][3]['text']:
+#                     block_size = True
+#     if rep_settings and block_size:
+#         return None
+#     else:
+#         return f"Failed (0/15): Failed tests marked false -- 1.4 replication settings check: {rep_settings}, 1.5 block size check: {block_size}"
 
-@test(points = 15, timeout = 100)
-def block_location_test():
-    for cell in parta_json_content[0]['cells']:
-        if cell["cell_type"]  == "code":
-            if "2.1" in cell['source']:
+# @test(points = 15, timeout = 100)
+# def block_location_test():
+#     for cell in parta_json_content[0]['cells']:
+#         if cell["cell_type"]  == "code":
+#             if "2.1" in cell['source']:
 
                 
-                output = cell['outputs'][0]['data']['text/plain']
-                output = output.strip("\,\{\} ").split("\n")
-                count_1 = int(output[0].split(":")[3].strip(" ,"))
-                count_2 = int(output[1].split(":")[3])
-                if count_1 + count_2 == 167:
-                    return None
-    return f"Failed (0/15): Count 1 and Count2 did not add up to 167 -- count 1: {count_1}, count 2: {count_2}"
+#                 output = cell['outputs'][0]['data']['text/plain']
+#                 output = output.strip("\,\{\} ").split("\n")
+#                 count_1 = int(output[0].split(":")[3].strip(" ,"))
+#                 count_2 = int(output[1].split(":")[3])
+#                 if count_1 + count_2 == 167:
+#                     return None
+#     return f"Failed (0/15): Count 1 and Count2 did not add up to 167 -- count 1: {count_1}, count 2: {count_2}"
 
-@test(points = 15, timeout = 100)
-def hdfs_count_test():
-    s_family = False
-    s_single = False
+# @test(points = 15, timeout = 100)
+# def hdfs_count_test():
+#     s_family = False
+#     s_single = False
 
-    d_family = False
-    d_single = False
-    for cell in parta_json_content[0]['cells']:
-        if cell["cell_type"]  == "code":
-            if "3.1" in cell['source']:
-                s_single = int(cell['outputs'][0]['data']['text/plain']) == 444874
-            if "3.2" in cell['source']:
-                s_family = int(cell['outputs'][0]['data']['text/plain']) == 2493
-            if "3.4" in cell['source']:
-                d_single = int(cell['outputs'][0]['data']['text/plain']) == 444874
-            if "3.5" in cell['source']:
-                d_family = int(cell['outputs'][0]['data']['text/plain']) == 2493
+#     d_family = False
+#     d_single = False
+#     for cell in parta_json_content[0]['cells']:
+#         if cell["cell_type"]  == "code":
+#             if "3.1" in cell['source']:
+#                 s_single = int(cell['outputs'][0]['data']['text/plain']) == 444874
+#             if "3.2" in cell['source']:
+#                 s_family = int(cell['outputs'][0]['data']['text/plain']) == 2493
+#             if "3.4" in cell['source']:
+#                 d_single = int(cell['outputs'][0]['data']['text/plain']) == 444874
+#             if "3.5" in cell['source']:
+#                 d_family = int(cell['outputs'][0]['data']['text/plain']) == 2493
     
 
-    if s_family and s_single and d_family and d_single:
-        return None
-    return f"Failed (0/15): Incorrect counts marked false -- singe.csv \"single\" line count {s_single}, singe.csv \"family\" line count {s_family} \
-            double.csv \"single\" line count {d_single}, double.csv \"family\" line count {d_family}"
+#     if s_family and s_single and d_family and d_single:
+#         return None
+#     return f"Failed (0/15): Incorrect counts marked false -- singe.csv \"single\" line count {s_single}, singe.csv \"family\" line count {s_family} \
+#             double.csv \"single\" line count {d_single}, double.csv \"family\" line count {d_family}"
 
-@test(points = 15, timeout = 100)
-def different_buffer_size_tests():
+# @test(points = 15, timeout = 100)
+# def different_buffer_size_tests():
     
-    for cell in parta_json_content[0]['cells']:
-        if cell["cell_type"]  == "code":
-            if "3.3" in cell['source']:
-                t1 = float(cell['outputs'][0]['data']['text/plain'])
-            if "3.6" in cell['source']:
-                t2 = float(cell['outputs'][0]['data']['text/plain'])
-            if "3.7" in cell['source']:
-                temp =  cell['outputs'][0]['data']['text/plain'].strip(" () ").split(",")
-                bs1 = int(temp[0].strip(" "))
-                bs2 = int(temp[1].strip(" "))
-    if (t1 != t2 and bs1 != bs2):
-                return None
+#     for cell in parta_json_content[0]['cells']:
+#         if cell["cell_type"]  == "code":
+#             if "3.3" in cell['source']:
+#                 t1 = float(cell['outputs'][0]['data']['text/plain'])
+#             if "3.6" in cell['source']:
+#                 t2 = float(cell['outputs'][0]['data']['text/plain'])
+#             if "3.7" in cell['source']:
+#                 temp =  cell['outputs'][0]['data']['text/plain'].strip(" () ").split(",")
+#                 bs1 = int(temp[0].strip(" "))
+#                 bs2 = int(temp[1].strip(" "))
+#     if (t1 != t2 and bs1 != bs2):
+#                 return None
     
-    return f"Failed (0/10): Either your times or your buffer sizes matched -- time1: {t1}, time2: {t2}, buffer size 1: {bs1}, buffer size 2: {bs2}"
+#     return f"Failed (0/10): Either your times or your buffer sizes matched -- time1: {t1}, time2: {t2}, buffer size 1: {bs1}, buffer size 2: {bs2}"
 
-@test(points=10)
-def dfs_admin_test():
-    for cell in part2_json_content[0]['cells']:
-        if cell["cell_type"]  == "code":
-            if "4.1" in cell['source']:
+# @test(points=10)
+# def dfs_admin_test():
+#     for cell in part2_json_content[0]['cells']:
+#         if cell["cell_type"]  == "code":
+#             if "4.1" in cell['source']:
 
-                for outputs in cell['outputs']:
-                    if "Dead datanodes (1):" in outputs['text']:
-                        return None
-    return "FAILED (0/10): hdfs dfsadmin -fs hdfs://main:9000/ -report did not show a dead datanode"
+#                 for outputs in cell['outputs']:
+#                     if "Dead datanodes (1):" in outputs['text']:
+#                         return None
+#     return "FAILED (0/10): hdfs dfsadmin -fs hdfs://main:9000/ -report did not show a dead datanode"
 
-@test(points=10)
-def newline_test():
-    for cell in part2_json_content[0]['cells']:
-        if cell["cell_type"]  == "code":
-            if "4.2" in cell['source']:
-                if int(cell['outputs'][0]['data']['text/plain']) > 0:
-                    return None
-    return "FAILED (0/10): no newline replacments detected"
+# @test(points=10)
+# def newline_test():
+#     for cell in part2_json_content[0]['cells']:
+#         if cell["cell_type"]  == "code":
+#             if "4.2" in cell['source']:
+#                 if int(cell['outputs'][0]['data']['text/plain']) > 0:
+#                     return None
+#     return "FAILED (0/10): no newline replacments detected"
 
-@test(points=10)
-def post_diaster_count_test():
-    s_family = False
-    s_single = False
+# @test(points=10)
+# def post_diaster_count_test():
+#     s_family = False
+#     s_single = False
 
-    d_family = False
-    d_single = False
+#     d_family = False
+#     d_single = False
 
-    for cell in part2_json_content[0]['cells']:
-        if cell["cell_type"]  == "code":
-            if "4.3" in cell['source']:
-                if 10000 < int(cell['outputs'][0]['data']['text/plain']) < 444874:
-                    s_single = True 
-            if "4.4" in cell['source']:
-                if 500 < int(cell['outputs'][0]['data']['text/plain']) < 2493:
-                    s_family = True
-            if "4.5" in cell['source']:
-                d_single = int(cell['outputs'][0]['data']['text/plain']) == 444874
-            if "4.6" in cell['source']:
-                d_family = int(cell['outputs'][0]['data']['text/plain']) == 2493
-    if s_family and s_single and d_family and d_single:
-        return None
-    return f"FAILED (0/10): Incorrect counts marked false -- singe.csv \"single\" line count {s_single}, singe.csv \"family\" line count {s_family} \
-            double.csv \"single\" line count {d_single}, double.csv \"family\" line count {d_family}"
+#     for cell in part2_json_content[0]['cells']:
+#         if cell["cell_type"]  == "code":
+#             if "4.3" in cell['source']:
+#                 if 10000 < int(cell['outputs'][0]['data']['text/plain']) < 444874:
+#                     s_single = True 
+#             if "4.4" in cell['source']:
+#                 if 500 < int(cell['outputs'][0]['data']['text/plain']) < 2493:
+#                     s_family = True
+#             if "4.5" in cell['source']:
+#                 d_single = int(cell['outputs'][0]['data']['text/plain']) == 444874
+#             if "4.6" in cell['source']:
+#                 d_family = int(cell['outputs'][0]['data']['text/plain']) == 2493
+#     if s_family and s_single and d_family and d_single:
+#         return None
+#     return f"FAILED (0/10): Incorrect counts marked false -- singe.csv \"single\" line count {s_single}, singe.csv \"family\" line count {s_family} \
+#             double.csv \"single\" line count {d_single}, double.csv \"family\" line count {d_family}"
 
 
 if __name__ == "__main__":
