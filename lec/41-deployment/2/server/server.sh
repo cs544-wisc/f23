b667cda33eaf917687bb0292062efa9e5578d238
Tyler Caraza-Harter <tharter@wisc.edu>
2023-12-11 20:31:47 +0000

    lec 41 code

--- lec/41-deployment/2/server/server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo hello
-mkdir test
-cd test
-hostname > index.html
-python3 -m http.server
--- lec/41-deployment/server.sh
+++ lec/41-deployment/server.sh
@@ -3,4 +3,4 @@ echo hello
 mkdir test
 cd test
 hostname > index.html
-python3 -m http.server
+python3 -m http.server 80
